4e6f524c94bcfba922934164cc7a18a30911a783
NwaguChima <nwaguchima48@gmail.com>
2022-07-03 23:43:52 +0100

    setup nav structure

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="">
+        <p>About</p>
+        <p>Store</p>
+      </div>
+      <div>
+        <p>Gmail</p>
+        <p>Images</p>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
