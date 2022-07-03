7a6e25ab13910c9614f7215fe071468d424e9c7a
NwaguChima <nwaguchima48@gmail.com>
2022-07-03 23:46:51 +0100

    setup basic  header style

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ import User from "./User";
 
 const Header = () => {
   return (
-    <header>
-      <div className="">
+    <header className="flex justify-between p-5 text-sm text-gray-700">
+      <div className="flex space-x-4  items-center">
         <p>About</p>
         <p>Store</p>
       </div>
-      <div>
+      <div className="flex space-x-4  items-center">
         <p>Gmail</p>
         <p>Images</p>
         <User />
