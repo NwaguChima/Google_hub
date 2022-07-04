0255764dd6fa5858e710318b6c713d52729c108d
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 01:51:26 +0100

    completed the user section

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,9 +6,9 @@ const signin = ({ providers }) => {
   return (
     <>
       <Header />
-      <div className="">
+      <div className="mt-40">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name} className="">
+          <div key={provider.name} className="flex flex-col items-center p-5">
             <picture>
               <source
                 srcSet="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
@@ -17,10 +17,14 @@ const signin = ({ providers }) => {
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
                 alt="provider logo"
+                className="w-55 object-cover"
               />
             </picture>
-            <p>Created for learning purposes</p>
+            <p className="text-sm italic my-10 text-center">
+              Created for learning purposes
+            </p>
             <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
               onClick={() =>
                 signIn(provider.id, {
                   callbackUrl: "/",
