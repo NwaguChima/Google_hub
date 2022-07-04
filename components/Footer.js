6d7a7d804f36ae70426c0383f39f9b24d1175e64
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 17:21:57 +0100

    completed footer section

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Copyright &copy;</p>
+    <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
+      <p>Copyright &copy; {new Date().getFullYear()} Chima Nwagu</p>
     </footer>
   );
 };
