51a230a3b8b1e49224a4217bb8da64ec70f752ae
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 01:25:01 +0100

    completed signin functionality

--- components/User.js
+++ components/User.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const User = () => {
-  return <div>User</div>;
+  const { data: session } = useSession();
+
+  if (session) {
+    return <>Sign out</>;
+  }
+  return (
+    <>
+      <button onClick={signIn}>Sign in</button>
+    </>
+  );
 };
 
 export default User;
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
 const signin = ({ providers }) => {
   return (
@@ -20,7 +20,15 @@ const signin = ({ providers }) => {
               />
             </picture>
             <p>Created for learning purposes</p>
-            <button>Sign in with {provider.name}</button>
+            <button
+              onClick={() =>
+                signIn(provider.id, {
+                  callbackUrl: "/",
+                })
+              }
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
