082cf5464b217dc2fcaa1c3381e421f70496ca7d
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 02:05:31 +0100

    setup the body form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import Image from "next/image";
 import Header from "../components/Header";
+import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
 
 export default function Home() {
   return (
@@ -14,6 +16,21 @@ export default function Home() {
       <Header />
 
       {/* Body */}
+      <form>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
+          width={300}
+          height={100}
+          alt="Google Hub Logo"
+        />
+        <div className="">
+          <SearchIcon className="h-5" />
+          <input type="text" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <button>Google Search</button>
+        <button>I&apos;m Feeling Lucky</button>
+      </form>
 
       {/* Footer */}
     </div>
