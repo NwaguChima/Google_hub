d6f12b9dba8a4a9b5c2ec7d28e7db9e413b3963e
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 17:37:53 +0100

    setup search page

---
+++ pages/search.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import React from "react";
+
+const search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Page</title>
+      </Head>
+
+      {/* Seach Header */}
+
+      {/* Search Result */}
+    </div>
+  );
+};
+
+export default search;
