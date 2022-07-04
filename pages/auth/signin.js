301be5634ee9dbe008149d3964d413bdf6cca584
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 01:14:43 +0100

    fetching provider

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Header from "../../components/Header";
+import { getProviders } from "next-auth/react";
 
-const signin = () => {
-  return <div>signIn</div>;
+const signin = ({ providers }) => {
+  return (
+    <>
+      <Header />
+      <div className="">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="">
+            <picture>
+              <source
+                srcSet="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
+                type="image/webp"
+              />
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
+                alt="provider logo"
+              />
+            </picture>
+            <p>Created for learning purposes</p>
+            <button>Sign in with {provider.name}</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: { providers },
+  };
+}
 
 export default signin;
