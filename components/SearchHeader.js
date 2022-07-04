a53c96dc2eb8a2ea0aeb8005b9365600807e2598
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 17:46:08 +0100

    passing input value from url successful

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,7 +1,34 @@
-import React from "react";
+import Image from "next/image";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 const SearchHeader = () => {
-  return <div>SearchHeader</div>;
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="">
+        <Image
+          onClick={() => router.push("/")}
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
+          width={120}
+          height={40}
+          objectFit="contain"
+          alt="Google Hub Logo"
+          className="cursor-pointer"
+        />
+        <form>
+          <input
+            type="text"
+            defaultValue={router.query.term}
+            ref={searchInputRef}
+            className=""
+          />
+        </form>
+      </div>
+    </header>
+  );
 };
 
 export default SearchHeader;
