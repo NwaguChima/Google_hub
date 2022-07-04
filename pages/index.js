a7aba373bfbc46f0b357a4c406d16e421cda945f
NwaguChima <nwaguchima48@gmail.com>
2022-07-04 17:30:48 +0100

    setup search function

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import Image from "next/image";
 import Header from "../components/Header";
 import { MicrophoneIcon, SearchIcon } from "@heroicons/react/solid";
 import Footer from "../components/Footer";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term.trim()) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <div>
       <Head>
@@ -26,11 +40,17 @@ export default function Home() {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full  items-center sm:max-w-xl lg:max-w-2xl">
           <SearchIcon className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow  focus:outline-none" />
+          <input
+            type="text"
+            className="flex-grow  focus:outline-none"
+            ref={searchInputRef}
+          />
           <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={handleSearch} className="btn">
+            Google Search
+          </button>
           <button className="btn">I&apos;m Feeling Lucky</button>
         </div>
       </form>
