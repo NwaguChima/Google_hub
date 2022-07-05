ee19ae976350ae44c49b77f6fe8595ef233d9ef7
NwaguChima <nwaguchima48@gmail.com>
2022-07-05 03:11:49 +0100

    completed the input styling

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -10,7 +10,7 @@ const SearchHeader = () => {
 
   return (
     <header className="sticky top-0 bg-white">
-      <div className="">
+      <div className="flex w-full p-6 items-center">
         <Image
           onClick={() => router.push("/")}
           src="https://upload.wikimedia.org/wikipedia/commons/6/6e/Google_Name.svg"
@@ -20,18 +20,18 @@ const SearchHeader = () => {
           alt="Google Hub Logo"
           className="cursor-pointer"
         />
-        <form>
+        <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
           <input
             type="text"
             defaultValue={router.query.term}
             ref={searchInputRef}
-            className=""
+            className="w-full focus:outline-none "
           />
-          <XIcon className="h-7" />
-          <MicrophoneIcon className="h-6" />
-          <SearchIcon className="h-6" />
+          <XIcon className="h-7 text-gray-500 cursor-pointer sm:mr-3" />
+          <MicrophoneIcon className="h-6 sm: inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
+          <SearchIcon className="h-6 sm: inline-flex text-blue-500" />
         </form>
-        <User />
+        <User className="ml-auto whitespace-nowrap" />
       </div>
     </header>
   );
