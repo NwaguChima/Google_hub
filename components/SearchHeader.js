4fcd43619cbfe23e104b110647761b22d7ebb2d3
NwaguChima <nwaguchima48@gmail.com>
2022-07-05 03:16:08 +0100

    completed search header for search page

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -8,6 +8,15 @@ const SearchHeader = () => {
   const router = useRouter();
   const searchInputRef = useRef(null);
 
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term.trim()) return;
+
+    router.push(`/search?term=${term.trim()}`);
+  };
+
   return (
     <header className="sticky top-0 bg-white">
       <div className="flex w-full p-6 items-center">
@@ -27,9 +36,13 @@ const SearchHeader = () => {
             ref={searchInputRef}
             className="w-full focus:outline-none "
           />
-          <XIcon className="h-7 text-gray-500 cursor-pointer sm:mr-3" />
+          <XIcon
+            onClick={() => (searchInputRef.current.value = "")}
+            className="h-7 text-gray-500 cursor-pointer sm:mr-3"
+          />
           <MicrophoneIcon className="h-6 sm: inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
           <SearchIcon className="h-6 sm: inline-flex text-blue-500" />
+          <button onClick={handleSearch} type="submit" hidden></button>
         </form>
         <User className="ml-auto whitespace-nowrap" />
       </div>
